64926e895a002772b9780bb5a2989495044cc360
Ivan Ristov <iristov95@gmail.com>
2019-03-27 13:14:18 +1100

    added an ansible folder for playbooks and inventory to install nginx as we previously were

--- terraform/template_files/userdata.sh
+++ terraform/template_files/userdata.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-sudo amazon-linux-extras install nginx1.12 -y
-sudo service nginx start
+echo "Installing pip"
+sudo yum install -y python-pip
+echo "Installing ansible via pip"
+sudo pip install ansible
+echo "Installing git"
+sudo yum install -y git
+echo "Cloning git repo"
+git clone https://github.com/soony88/kafka-playground.git
+echo "Running playbook"
+ansible-playbook kafka-playground/ansible/playbook_local.yml
